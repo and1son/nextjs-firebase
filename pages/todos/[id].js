77f71b6aa5aaa15be006d102787a60926051b967
and1son <andi.basic10@gmail.com>
2021-11-29 17:37:36 +0100

    getStaticPaths, getStaticProps detail

--- pages/todos/[id].js
+++ pages/todos/[id].js
@@ -1,6 +1,7 @@
-import { Grid } from "@mui/material"
+import { Grid, Typography, Card, CardContent, CardActions, Button } from "@mui/material"
 import { doc, getDoc, getDocs, collection } from "@firebase/firestore"
 import { db } from "../../firebase"
+import Link from 'next/link'
 
 const Detail = ({ todoProps }) => {
     const todo = JSON.parse(todoProps)
@@ -13,7 +14,27 @@ const Detail = ({ todoProps }) => {
             justifyContent="center"
             style={{ minHeight: '100vh' }}
         >
-            {todo.title} : {todo.detail}
+            <Grid item xs={5}>
+                <Card sx={{ minWidth: 300, maxWidth: 500, boxShadow: 4, }}
+                    style={{ backgroundColor: '#fafafa' }}
+                >
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            {todo.title}
+                        </Typography>
+
+                        <Typography sx={{ mb: 1 }} color="text.secondary">
+                            {todo.detail}
+                        </Typography>
+
+                    </CardContent>
+                    <CardActions>
+                        <Link href="/">
+                            <Button size="small"> Back to home</Button>
+                        </Link>
+                    </CardActions>
+                </Card>
+            </Grid>
         </Grid>
     )
 }
